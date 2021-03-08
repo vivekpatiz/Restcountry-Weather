c740aa0f077e002bad0d81f5c2de89579424fd12
Vivekanandan <41379292+vivekpatiz@users.noreply.github.com>
2021-03-08 11:23:11 +0530

    Update restcountry.js

--- restcountry.js
+++ restcountry.js
@@ -1,59 +1,70 @@
+var data = fetch("https://restcountries.eu/rest/v2/all");
 
+data
+  .then( (response)=> {
+    return response.json();
+  })
+  .then((response)=> {
+    restcountries(response)
+    console.log(response)
+})
+  .catch(function (err) {
+    console.log(err);
+  });
 
-fetch('https://restcountries.eu/rest/v2/all')
-.then(response => response.json())
-.then(data =>
-    {
-    for(i in data){
-        var bd = creDOM('div','card wid','');
-        var imgsrc = data[i].flag;
-        var img = creDOM('img','card-img-top','',imgsrc);
-        var cardbody = creDOM('div','card-body');
-        var tit = data[i].name;
-        var wea = [];
-        var name = title('h4','card-title text-center','',tit);
-        var capp = "<b>Capital: </b>"+ data[i].capital;
-        var cap = title('p','card-text text-center','',capp);
-        var reg = "<b>Region: </b>"+ data[i].region;
-        var region = title('p','card-text text-center','',reg);
-        var cd = "<b>Country Code: </b>"+ data[i].alpha3Code;
-        var code = title('p','card-text text-center','',cd);
-        var btn_center = document.createElement('div');
-        btn_center.setAttribute('class','text-center'); 
-        {var sol = data[i].name;
-        var val = 'http://api.openweathermap.org/data/2.5/weather?q='+sol+'&appid=c15b1648ee4adfdc9a5660cff0af3885';
-        wea[i] = val;
-        }
-        var ar = title('input','btn btn-dark',i,'weather','button','weather()');
-        btn_center.appendChild(ar);
-        cardbody.append(name,cap,region,code,btn_center);
-        bd.append(img,cardbody);
-        document.body.append(bd); 
-        
-      
+function restcountries(response){
+for(i in response){
+    createdata(i,response)
 }
-    
-    function creDOM(eleName,eleClass,eleId,elesrc){
-        var ele = document.createElement(eleName);
-        ele.setAttribute("class",eleClass);
-        ele.setAttribute('id',eleId);
-        ele.setAttribute('src',elesrc);
-        return ele;
-    }
-    function weather(){
-        alert('http://api.openweathermap.org/data/2.5/weather?q='+tit+'&appid=c15b1648ee4adfdc9a5660cff0af3885');
-    }
-    function title(eleName,eleClass,eleId,eletit,eletype,elefunc){
-        var ele = document.createElement(eleName);
-        ele.setAttribute("class",eleClass);
-        ele.setAttribute('id',eleId);
-        ele.setAttribute('value',eletit);
-        ele.setAttribute('type',eletype);
-        ele.innerHTML = eletit;
-        ele.setAttribute('onclick',elefunc);
-        return ele;
-    }
-   
-    }
-);
+ 
+}
+
+var container = document.createElement("div");
+container.setAttribute("class", "container bg");
+var row = document.createElement("div");
+row.setAttribute("class", "row");
+function createdata(i,data){
+var col = document.createElement("div");
+col.setAttribute("class", "col-lg-4 card shadow");
+var header = document.createElement("div");
+header.setAttribute("class", "card-header");
+header.innerText = data[i].name;
+var body = document.createElement("div");
+body.setAttribute("class", "card-body");
+var image = document.createElement("img");
+image.src = data[i].flag;
+var capital = document.createElement("div");
+capital.innerText = "Capital :"+" "+ data[i].capital;
+var region = document.createElement("div");
+region.innerText = "Region :"+" " + data[i].region;
+var countrycode = document.createElement("div");
+countrycode.innerText ="Country Code :"+" "+ data[i].alpha3Code;
+var button =  document.createElement("button");
+var weathercountry = data[i].capital;
+button.setAttribute("class", "btn btn-warning");
+button.innerText = "Weather";
+button.addEventListener('click',function(){
+    var result = fetch('https://api.openweathermap.org/data/2.5/weather?q='+weathercountry+'&appid=450767ab95b7a0a89f79899717edcde5')
+
+result
+.then( (weatherresult)=> {
+    return weatherresult.json();
+  })
+  .then((weatherresult)=> {
+    alert(+weatherresult.main.temp+'\n'+ +weatherresult.coord.lon+'\n'+ + weatherresult.coord.lat )
+})
+  .catch(function (err) {
+    alert("err");
+  });
+
+
+});
+
+body.append(image, capital, region, countrycode,button);
+col.append(header, body );
+row.append(col);
+container.append(row);
+document.body.append(container);
+
+}
 
